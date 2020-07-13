0f8a1ba4aa0538a2a832b25e04fd5aefa9228c5c
fabrynet <fabrynet.bianco@gmail.com>
2020-07-13 15:56:31 +0200

    Invio messaggio su thread chat.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ function sendMessage () {
   var message = $('#inputMessage').val();
   console.log(message);
   var btn = $('#btnMessage');
-  var boxMessageUser = $('.template-box-message.user');
+  var boxMessageUser = $('.box-message.user');
   var cloneBoxMessageUser = boxMessageUser.clone();
   btn.click(function(){
     var messageUser = cloneBoxMessageUser.text(message);
     console.log(messageUser);
-    $('.list-chat').append(messageUser);
+    $('.thread-chat').append(messageUser);
   });
 }
 
