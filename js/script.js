4ae9e3772f6b6a78b2b12be195f54a0ba2f4b5f3
fabrynet <fabrynet.bianco@gmail.com>
2020-07-13 22:53:41 +0200

    Risposta ok da interlocutore. Invio messaggio tramite invio tastiera.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 function sendMessage () {
+  var message = $('#input-message');
+  var boxMessageUser = $('.box-message.user').clone();
+  var boxMessageInterlocutor = $('.box-message.interlocutor').clone();
+  console.log(message.val());
+  console.log(boxMessageUser);
+  if (message.val()!="") {
+    
+    var messageUser = boxMessageUser.text(message.val());
+    $('.chat-thread').append(messageUser);
 
-  var btn = $('#btnMessage');
-  var boxMessageUser = $('.box-message.user');
+    setTimeout(function(){
+      var messageInterlocutor = boxMessageInterlocutor.text('ok');
+      $('.chat-thread').append(messageInterlocutor);
+    }, 1000)
 
-  btn.click(function(){
-    var message = $('#inputMessage').val();
-    var cloneBoxMessageUser = boxMessageUser.clone();
-    console.log(message);
-    if (message!="") {
-      var messageUser = cloneBoxMessageUser.text(message);
-      console.log(messageUser);
-      $('.chat-thread').append(messageUser);
-    }
-  });
+  }
+  message.val('');
+}
+
+function eventEnter () {
+  var keyWhich = event.which;
+  var keyCode = event.keyCode;
+  if (keyWhich == 13 || keyCode == 13) {
+    sendMessage();
+  }
+}
+
+function inputMessage () {
+
+  var btn = $('#btn-message');
+  var inputMessage = $('#input-message');
+
+  btn.click(sendMessage);
+  inputMessage.keyup(eventEnter);
 }
 
 function init () {
-  sendMessage();
+  inputMessage();
 }
 
 $(document).ready(init);
