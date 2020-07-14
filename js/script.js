1f72208638b845ffb05cd1e4ed6b8837c51de7c9
Fabrizio <fabrynet.bianco@gmail.com>
2020-07-14 18:40:28 +0200

    Milestone 2. Ricerca dei nomi dei contatti.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,35 @@
 // Functions Procedurali
+function addSearchListener () {
+  // scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+  // confronto le lettere inserite nell'input di ricerca con i nomi presenti nella lista dei nomi
+  var searchInput = $('#input-search');
+  searchInput.keyup(searchKeyup);
+}
+
+function searchKeyup() {
+  // prendo le lettere inserite nella barra di ricerca
+  var query = $(this).val();
+  console.log(query);
+  // le confronto con le lettere presenti nei nomi dei contatti
+  $('.contacts-list .contact').each(function(){
+    var name = $(this).find('.title').html();
+    console.log(name);
+    var resultQuery = name.search(query);
+    console.log('result Query',resultQuery);
+    // se non ci sono delle corrispondenze nascondo quel nome attraverso la classe .hidden
+    if (resultQuery == -1) {
+      console.log('trovata corrispondenza', name.search(query));
+      $(this).addClass('hidden');
+    } else {
+      $(this).removeClass('hidden');
+    }
+    // se la barra di ricerca è vuota rimuovo tutti gli .hidden
+    if (!query) {
+      $(this).removeClass('hidden');
+    }
+  });
+}
+
 function addSendListener(){
   var targetInput = $('#input-message');
   var targetButton = $('#button-message');
@@ -18,7 +49,7 @@ function sendKeyup(event) {
   }
 }
 
-function sendClick () {
+function sendClick() {
   var input = $('#input-message');
   var txt = input.val();
   if (txt) {
@@ -28,7 +59,7 @@ function sendClick () {
   }
 }
 
-function sendMessage (txt) {
+function sendMessage(txt) {
 
   var time = getActualHour();
   var templateSent = $('.template .message-box.sent').clone();
@@ -39,7 +70,7 @@ function sendMessage (txt) {
 
 }
 
-function receiveMessage () {
+function receiveMessage() {
 
   var time = getActualHour();
   var templateReceived = $('.template .message-box.received').clone();
@@ -51,8 +82,9 @@ function receiveMessage () {
   }, 1000);
 }
 
-function init () {
+function init() {
   addSendListener();
+  addSearchListener();
 }
 
 $(document).ready(init);
