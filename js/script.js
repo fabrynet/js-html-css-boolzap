c993b87fe2d0611807e32a7bee0769b20d7c136d
fabrynet <fabrynet.bianco@gmail.com>
2020-07-14 09:06:11 +0200

    Stili sui thread delle chat

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 function sendMessage () {
+
   var message = $('#input-message');
-  var boxMessageUser = $('.box-message.user').clone();
-  var boxMessageInterlocutor = $('.box-message.interlocutor').clone();
-  console.log(message.val());
-  console.log(boxMessageUser);
+
   if (message.val()!="") {
-    
-    var messageUser = boxMessageUser.text(message.val());
+
+    var d = new Date();
+    var time = d.getHours() + ":" + d.getMinutes();
+    console.log(time);
+    var timeMessageUser = $('.time-message.user').html(time);
+    var timeMessageInterlocutor = $('.time-message.interlocutor').html(time);
+
+    var boxMessageUser = $('.box-message.user').clone();
+    var boxMessageInterlocutor = $('.box-message.interlocutor').clone();
+
+    console.log(message);
+    console.log(message.val());
+    console.log(boxMessageUser);
+
+    var messageUser = boxMessageUser.html(message.val());
     $('.chat-thread').append(messageUser);
 
     setTimeout(function(){
-      var messageInterlocutor = boxMessageInterlocutor.text('ok');
+      var messageInterlocutor = boxMessageInterlocutor.html('ok');
       $('.chat-thread').append(messageInterlocutor);
     }, 1000)
 
@@ -28,10 +39,10 @@ function eventEnter () {
 
 function inputMessage () {
 
-  var btn = $('#btn-message');
+  var btnMessage = $('#btn-message');
   var inputMessage = $('#input-message');
 
-  btn.click(sendMessage);
+  btnMessage.click(sendMessage);
   inputMessage.keyup(eventEnter);
 }
 
