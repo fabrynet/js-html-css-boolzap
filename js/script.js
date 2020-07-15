e1038dac92313b9ec734fa5bd207d4a9c0ab5002
Fabrizio <fabrynet.bianco@gmail.com>
2020-07-15 12:32:25 +0200

    Riorganizzazione funzioni.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,42 @@
 // Functions Procedurali
-function addSearchListener () {
-  // scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
-  // confronto le lettere inserite nell'input di ricerca con i nomi presenti nella lista dei nomi
+function addListeners () {
+
+  var targetInput = $('#input-message');
+  var targetButton = $('#button-message');
   var searchInput = $('#input-search');
+
+  targetInput.keyup(sendKeyup);
+  targetButton.click(sendClick);
   searchInput.keyup(searchKeyup);
 }
 
 function searchKeyup() {
   // prendo le lettere inserite nella barra di ricerca
-  var query = $(this).val();
-  console.log(query);
+  var input = $(this);
+  var query = input.val().toLowerCase();
+  var contacts = $('.contacts-list .contact');
   // le confronto con le lettere presenti nei nomi dei contatti
-  $('.contacts-list .contact').each(function(){
-    var name = $(this).find('.title').html().toLowerCase();
-    console.log(name);
-    var resultQuery = name.search(query);
-    console.log('result Query',resultQuery);
-    // se non ci sono delle corrispondenze nascondo quel nome attraverso la classe .hidden
-    if (resultQuery == -1) {
-      console.log('trovata corrispondenza', name.search(query));
-      $(this).addClass('hidden');
+  contacts.each(function(){
+    var contact = $(this);
+    var name = contact.find('.title').text().toLowerCase();
+
+    if (name.includes(query)) {
+      contact.show();
     } else {
-      $(this).removeClass('hidden');
+      contact.hide();
     }
-    // se la barra di ricerca è vuota rimuovo tutti gli .hidden
-    if (!query) {
-      $(this).removeClass('hidden');
-    }
+    // var resultQuery = name.search(query);
+    // // se non ci sono delle corrispondenze nascondo quel nome attraverso la classe .hidden
+    // if (resultQuery == -1) {
+    //   console.log('trovata corrispondenza', name.search(query));
+    //   $(this).addClass('hidden');
+    // } else {
+    //   $(this).removeClass('hidden');
+    // }
+    // // se la barra di ricerca è vuota rimuovo tutti gli .hidden
+    // if (!query) {
+    //   $(this).removeClass('hidden');
+    // }
   });
 }
 
@@ -43,9 +53,7 @@ function sendKeyup(event) {
   var keyWhich = event.which;
   var keyCode = event.keyCode;
   if ( (keyWhich == 13 || keyCode == 13) && txt) {
-    input.val('');
-    sendMessage(txt);
-    receiveMessage();
+    sendChat(input, txt);
   }
 }
 
@@ -53,38 +61,32 @@ function sendClick() {
   var input = $('#input-message');
   var txt = input.val();
   if (txt) {
-    input.val('');
-    sendMessage(txt);
-    receiveMessage();
+    sendChat(input, txt);
   }
 }
 
-function sendMessage(txt) {
-
-  var time = getActualHour();
-  var templateSent = $('.template .message-box.sent').clone();
-
-  templateSent.find('.message-time').text(time);
-  templateSent.find('.message-text').text(txt);
-  $('.chat-thread').append(templateSent);
-
+function sendChat(input, txt) {
+  input.val('');
+  sendMessage(txt, 'sent');
+  setTimeout(function(){ sendMessage('ok','received')}, 1000);
 }
 
-function receiveMessage() {
+function sendMessage(txt, type) {
 
   var time = getActualHour();
-  var templateReceived = $('.template .message-box.received').clone();
+  var templateMessage = $('.template .message-box').clone();
+  var target = $('.chat-thread');
 
-  setTimeout(function(){
-    templateReceived.find('.message-time').text(time);
-    templateReceived.find('.message-text').text('ok');
-    $('.chat-thread').append(templateReceived);
-  }, 1000);
+  templateMessage.addClass(type);
+
+  templateMessage.find('.message-time').text(time);
+  templateMessage.find('.message-text').text(txt);
+  $('.chat-thread').append(templateMessage);
+
 }
 
 function init() {
-  addSendListener();
-  addSearchListener();
+  addListeners();
 }
 
 $(document).ready(init);
